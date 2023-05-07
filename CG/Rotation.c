592e342950a26be78dc725e2454a8d7f21638eb0
Kamalika <59131714+doesnots@users.noreply.github.com>
2023-05-07 16:24:59 +0530

    Update Rotation.c

--- CG/Rotation.c
+++ CG/Rotation.c
@@ -1,19 +1,30 @@
-#include <stdio.h> 
-#include<graphics.h> 
-#include <math.h> 
-int main(){ 
-int gd = DETECT,gm; 
-initgraph(&gd, &gm, "C://TURBOC3/BGI"); 
-int x1,x2,y1,y2,theta; 
-printf("Enter the X Co-ordinate: "); scanf("%d , %d", &x1, &x2); 
-printf("Enter the Y Co-ordinate: "); scanf("%d , %d", &y1, &y2); 
-printf("Enter Theta"); 
-scanf("%d", &theta); 
-theta = 45*M_PI/180; 
-setcolor(RED); 
-line(x1,y1,x2,y2); 
-int x1_new, y1_new, x2_new, y2_new; x1_new = round(x1 * cos(theta) - y1*sin(theta)); y1_new = round(x1 * sin(theta) - y1*cos(theta)); x2_new = round(x2 * cos(theta) - y2*sin(theta)); y2_new = round(x2 * sin(theta) + y2*cos(theta)); setcolor(GREEN); 
-line(x1_new,y1_new,x2_new,y2_new); getch(); 
-closegraph(); 
-return 0; 
+#include <graphics.h>
+#include <math.h>
+#include <stdio.h>
+#include <conio.h>
+main() {
+ int x1,y1,x2,y2;
+ float angle,radians,s,c;
+ int x1_new,y1_new,x2_new,y2_new;
+ int gd = DETECT, gm;
+ initgraph(&gd, &gm, "");
+ printf("Enter coordinates of line\n");
+ scanf("%d %d %d %d",&x1,&y1,&x2,&y2);
+ line(x1, y1, x2, y2);
+ putpixel(x1,y1,GREEN);
+ putpixel(x2,y2,RED);
+ printf("Enter a rotating angle\n");
+ scanf("%f",&angle);
+ radians = angle * M_PI / 180;
+ s = sin(radians);
+ c = cos(radians);
+ x1_new =floor(x1 * c + y1 * s);
+ y1_new =floor(-x1 * s + y1 * c);
+ x2_new =floor(x2 * c + y2 * s);
+ y2_new =floor(-x2 *s + y2 * c);
+ line(x1_new, y1_new, x2_new, y2_new);
+ putpixel(x1_new,y1_new,GREEN);
+ putpixel(x2_new,y2_new,RED);
+ getch();
+ closegraph();
 }
