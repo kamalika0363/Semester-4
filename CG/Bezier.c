e7555ec8e2d7ec250d21951129f2f684968815d1
Kamalika <59131714+doesnots@users.noreply.github.com>
2023-05-08 08:20:33 +0530

    Update Bezier.c

--- CG/Bezier.c
+++ CG/Bezier.c
@@ -17,8 +17,8 @@ line(x2,y2,x3,y3);
 line(x3,y3,x4,y4);
 line(x4,y4,x1,y1);
 for(u=0.0;u<=1.0;u=u+0.001){
-put_x = pow(1-u,3)x1 + 3*u*pow(1-u,2)*x2 + 3*u*u(1-u)*x3 + pow(u,3)*x4;
-put_y = pow(1-u,3)y1 + 3*u*pow(1-u,2)*y2 + 3*u*u(1-u)*y3 + pow(u,3)*y4;
+put_x = pow(1-u,3)*x1 + 3*u*pow(1-u,2)*x2 + 3*u*u(1-u)*x3 + pow(u,3)*x4;
+put_y = pow(1-u,3)*y1 + 3*u*pow(1-u,2)*y2 + 3*u*u(1-u)*y3 + pow(u,3)*y4;
 putpixel(put_x,put_y,7);
 }
 getch();
